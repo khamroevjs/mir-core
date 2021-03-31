813817fd85b4c96cd7d2be90ae3465e7a3c646c2
SilentRemix <55449509+SilentRemix@users.noreply.github.com>
2021-04-01 00:12:39 +0300

    Update Platform.java

--- routing/src/main/java/mir/routing/emulator/Platform.java
+++ routing/src/main/java/mir/routing/emulator/Platform.java
@@ -28,7 +28,7 @@ public class Platform {
         this.service = service;
     }
 
-    private final String URI = "http://localhost:8080/api"; // TODO: change to actual URI.
+    private final String URI = "https://mir-issuer.herokuapp.com/main/api";
 
     private String sendRequest(String hex) {
         // Form new Http-request to Issuer and get response from it.
